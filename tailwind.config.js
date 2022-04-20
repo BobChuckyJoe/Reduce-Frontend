0f554b2dd3863c9f2b9af267d5e207d23711f477
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2022-04-20 04:59:45 -0500

    feat: add color to theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
-	content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      blue: "#073B4C",
+      white: "#E5E5E5",
+    },
     extend: {},
   },
   plugins: [],
-}
+};
