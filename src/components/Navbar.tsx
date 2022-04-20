4de3858b1c056adcbd1b760b8a20831f023e6f6b
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2022-04-20 05:04:44 -0500

    feat: add navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+const Bar = () => <div className="w-10 h-2 bg-white" />;
+
+interface NavbarProps {}
+
+const Navbar = (props: NavbarProps) => {
+  return (
+    <div className="bg-blue w-full h-20 flex flex-row justify-between items-center px-5">
+      <div className="flex flex-col justify-between space-y-2">
+        <Bar /> <Bar /> <Bar />
+      </div>
+      <h2 className="text-white font-extrabold text-3xl">My Stats</h2>
+    </div>
+  );
+};
+
+export default Navbar;
