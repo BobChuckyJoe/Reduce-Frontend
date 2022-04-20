5b6527e414e815db052be74d30a2d15f079c4bf9
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2022-04-20 09:09:08 -0500

    feat: make counters increase when clicked

--- src/components/TrashCounter.tsx
+++ src/components/TrashCounter.tsx
@@ -1,18 +1,25 @@
+import { useState } from "react";
+
 interface TrashCounterProps {
   icon: React.ReactElement;
   amount: number;
 }
 
 const TrashCounter = ({ icon, amount }: TrashCounterProps) => {
+  const [numTrash, setNumTrash] = useState(amount);
+
   return (
-    <div className="flex flex-row h-20 w-40">
+    <button
+      className="flex flex-row h-20 w-40"
+      onClick={() => setNumTrash((numTrash) => numTrash + 1)}
+    >
       <div className="rounded-l-full h-full w-1/2 bg-blue flex items-center justify-center">
         {icon}
       </div>
       <div className="rounded-r-full border-solid border-y-4 border-r-4 border-blue h-full w-1/2 bg-white flex items-center justify-center">
-        <p className="text-blue text-lg">{amount}</p>
+        <p className="text-blue font-bold text-3xl">{numTrash}</p>
       </div>
-    </div>
+    </button>
   );
 };
 
