c302f0306c33367bbb5eb632499b3cb417cfe130
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2022-04-20 09:08:46 -0500

    feat: add more styling to home screen

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,15 +1,31 @@
 import GraphPreview from "components/GraphPreview";
 import Navbar from "components/Navbar";
 import TrashCounter from "components/TrashCounter";
+import CanIcon from "icons/CanIcon";
+import PaperCupIcon from "icons/PaperCupIcon";
+import PaperIcon from "icons/PaperIcon";
+import WaterBottleIcon from "icons/WaterBottleIcon";
 
 interface HomeProps {}
 
 const Home = (props: HomeProps) => {
   return (
-    <div>
+    <div className="bg-white">
       <Navbar />
-      <GraphPreview />
-      <TrashCounter amount={7} icon={<p className="text-white">water</p>} />
+      <div className="p-4 w-full flex flex-col items-center">
+        <GraphPreview />
+        <div className="flex flex-row space-x-4 mb-4">
+          <TrashCounter amount={7} icon={<WaterBottleIcon />} />
+          <TrashCounter amount={7} icon={<CanIcon />} />
+        </div>
+        <div className="flex flex-row space-x-4 mb-4">
+          <TrashCounter amount={7} icon={<PaperCupIcon />} />
+          <TrashCounter amount={7} icon={<PaperIcon />} />
+        </div>
+        <div className="bg-blue rounded-full w-20 h-20 flex justify-center items-center">
+          <p className="text-white text-5xl font-bold">+</p>
+        </div>
+      </div>
     </div>
   );
 };
