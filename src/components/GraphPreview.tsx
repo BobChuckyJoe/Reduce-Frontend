d823563d8a1c215474271ee031c6d43213e1ae80
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2022-04-20 23:24:06 -0500

    fix: layout shift on graph

--- src/components/GraphPreview.tsx
+++ src/components/GraphPreview.tsx
@@ -6,23 +6,24 @@ interface GraphPreviewProps {
 
 const GraphPreview = ({ data }: GraphPreviewProps) => {
   return (
-    <Chart
-      chartType="LineChart"
-      width="300px"
-      height="150px"
-      data={data}
-      className="border-solid border-2 border-blue w-full"
-      options={{
-        backgroundColor: "#e5e5e5",
-        legend: "none",
-        colors: ["#073B4C"],
-        vAxis: {
-          gridlines: { color: "transparent" },
-          ticks: [],
-        },
-        hAxis: { gridlines: { color: "transparent" }, ticks: [] },
-      }}
-    />
+    <div className="h-[150px] w-[300px] border-solid border-2 border-blue">
+      <Chart
+        chartType="LineChart"
+        width="100%"
+        height="100%"
+        data={data}
+        options={{
+          backgroundColor: "#e5e5e5",
+          legend: "none",
+          colors: ["#073B4C"],
+          vAxis: {
+            gridlines: { color: "transparent" },
+            ticks: [],
+          },
+          hAxis: { gridlines: { color: "transparent" }, ticks: [] },
+        }}
+      />
+    </div>
   );
 };
 
